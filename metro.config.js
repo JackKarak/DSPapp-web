9fa1b012fa89e96afb1ccee8de9abc93ecb2e416
jkaraka <jkara@terpmail.umd.edu>
2025-09-11 13:29:16 -0400

    Try try again

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,11 @@
 const { getDefaultConfig } = require('expo/metro-config');
 
+/**
+ * Metro configuration for Expo
+ * https://docs.expo.dev/guides/customizing-metro
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
 const config = getDefaultConfig(__dirname);
 
 // Disable problematic features for React Native compatibility
