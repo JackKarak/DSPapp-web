d627d918f68a5d687e98a7c5e838b27f539b6cf6
jkaraka <jkara@terpmail.umd.edu>
2026-02-05 14:53:18 -0500

    Optimize web bundle - reduce size by excluding unused icon fonts

---
+++ metro.assetExts.js
@@ -0,0 +1,26 @@
+/**
+ * Asset exclusion configuration for Metro bundler
+ * Excludes unused icon fonts to reduce bundle size
+ */
+
+module.exports = {
+  excludedFonts: [
+    'AntDesign.ttf',
+    'Entypo.ttf',
+    'EvilIcons.ttf',
+    'Feather.ttf',
+    'FontAwesome.ttf',
+    'FontAwesome5_Brands.ttf',
+    'FontAwesome5_Regular.ttf',
+    'FontAwesome5_Solid.ttf',
+    'FontAwesome6_Brands.ttf',
+    'FontAwesome6_Regular.ttf',
+    'FontAwesome6_Solid.ttf',
+    'Fontisto.ttf',
+    'Foundation.ttf',
+    'MaterialCommunityIcons.ttf',
+    'Octicons.ttf',
+    'SimpleLineIcons.ttf',
+    'Zocial.ttf',
+  ],
+};
--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,6 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
+const { excludedFonts } = require('./metro.assetExts');
 
 /**
  * Metro configuration for Expo
@@ -14,10 +16,35 @@ config.resolver.platforms = ['ios', 'android', 'web'];
 // Platform-specific extensions resolution order
 config.resolver.sourceExts = [...config.resolver.sourceExts, 'web.ts', 'web.tsx', 'web.js', 'web.jsx'];
 
-// Disable problematic features for React Native compatibility
+// Exclude unused icon fonts to reduce bundle size
+config.resolver.assetExts = config.resolver.assetExts.filter(ext => ext !== 'ttf');
+config.resolver.assetExts.push('ttf');
+
+// Custom asset resolution to exclude fonts
+const defaultAssetExts = config.resolver.assetExts;
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  // Exclude unused fonts
+  if (moduleName.includes('vector-icons') && moduleName.includes('Fonts')) {
+    const fontName = moduleName.split('/').pop();
+    if (excludedFonts.includes(fontName)) {
+      return {
+        type: 'empty',
+      };
+    }
+  }
+  
+  return context.resolveRequest(context, moduleName, platform);
+};
+
+// Optimize for production
 config.transformer.minifierConfig = {
   mangle: {
-    keep_fnames: true,
+    keep_fnames: false,
+  },
+  compress: {
+    drop_console: true,
+    drop_debugger: true,
+    pure_funcs: ['console.log', 'console.info', 'console.debug', 'console.warn'],
   },
 };
 
